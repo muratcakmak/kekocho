0ca39c4980ebd4484d9f702041d3e28f297d574a
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-24 11:40:55 -0400

    Create user model, users, session and application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'session/create'
+  end
+
+  namespace :api do
+    get 'session/destroy'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
+  namespace :api do
+    get 'users/show'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
