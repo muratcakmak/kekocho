2f96fb9e95e70151354b1760e636d4945c5cf687
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-24 11:44:41 -0400

    Configure backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'session/create'
-  end
+  root to: 'static_pages#root'
 
-  namespace :api do
-    get 'session/destroy'
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
   end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  namespace :api do
-    get 'users/show'
-  end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
