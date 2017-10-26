eac07817d18a9e5623c5c5693429b1dfafecdd04
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-25 22:37:22 -0400

    Make improvements to styling

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -31710,6 +31710,10 @@ var _search_bar_container = __webpack_require__(230);
 
 var _search_bar_container2 = _interopRequireDefault(_search_bar_container);
 
+var _question_button = __webpack_require__(232);
+
+var _question_button2 = _interopRequireDefault(_question_button);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -31782,15 +31786,7 @@ var NavBar = function (_React$Component) {
             { className: 'header-search-container' },
             _react2.default.createElement(_search_bar_container2.default, null)
           ),
-          _react2.default.createElement(
-            'div',
-            { className: 'header-question-button-container' },
-            _react2.default.createElement(
-              'button',
-              { className: 'header-question-button' },
-              'Add Question'
-            )
-          )
+          _react2.default.createElement(_question_button2.default, null)
         )
       );
     }
@@ -31883,6 +31879,62 @@ var SearchBar = function (_React$Component) {
 }(_react2.default.Component);
 
 exports.default = SearchBar;
+
+/***/ }),
+/* 232 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouterDom = __webpack_require__(13);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var QuestionButton = function (_React$Component) {
+  _inherits(QuestionButton, _React$Component);
+
+  function QuestionButton() {
+    _classCallCheck(this, QuestionButton);
+
+    return _possibleConstructorReturn(this, (QuestionButton.__proto__ || Object.getPrototypeOf(QuestionButton)).apply(this, arguments));
+  }
+
+  _createClass(QuestionButton, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: 'header-question-button-container' },
+        _react2.default.createElement(
+          'button',
+          { className: 'header-question-button' },
+          'Add Question'
+        )
+      );
+    }
+  }]);
+
+  return QuestionButton;
+}(_react2.default.Component);
+
+exports.default = QuestionButton;
 
 /***/ })
 /******/ ]);
