fe9fa8cf789aeca690ab5100623bfec6c743ebbc
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 16:54:27 -0400

    Add ui reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 import ErrorsReducer from './errors_reducer';
 import SessionReducer from './session_reducer';
 import EntityReducer from './entity_reducer';
+import UIReducer from './ui_reducer';
+
 const RootReducer = combineReducers({
   session: SessionReducer,
   errors: ErrorsReducer,
-  entities: EntityReducer
+  entities: EntityReducer,
+  ui: UIReducer
 });
 
 
