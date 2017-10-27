306476e13680789734871a6d7e2afa348f804429
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 16:48:57 -0400

    Add add modal function to window for test

--- frontend/kekocho.jsx
+++ frontend/kekocho.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { login, logout, receiveErrors } from './actions/session_actions';
+import { addAskQuestionModal } from './actions/ui_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   window.login = login;
   window.logout = logout;
+  window.modal = {modal: {
+    show: true,
+    }
+  };
+  window.addAskQuestionModal = addAskQuestionModal;
   let store;
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
