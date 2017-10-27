252f531d4600576cda8020f22f500cd361cc52c4
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-26 21:34:09 -0400

    Add semi-colon for styling

--- frontend/kekocho.jsx
+++ frontend/kekocho.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { login, logout, receiveErrors } from './actions/session_actions'
+import { login, logout, receiveErrors } from './actions/session_actions';
 document.addEventListener('DOMContentLoaded', () => {
-
   const root = document.getElementById('root');
-
   window.login = login;
   window.logout = logout;
-  
   let store;
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
