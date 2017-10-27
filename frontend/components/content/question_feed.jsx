23a78cd6a6ffd52cf4175345b86a7a3b1e47ab0e
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-26 21:28:35 -0400

    Implement request question ability to persisted user

--- frontend/components/content/question_feed.jsx
+++ frontend/components/content/question_feed.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import QuestionIndexItem from './question_item_index';
 
 class QuestionFeed extends React.Component{
+
+  componentDidMount(){
+    this.props.requestQuestions();
+  }
   render(){
+    const questionItems = [];
+    const questionIndexItems = [];
+    if(this.props.entities.hasOwnProperty("questions")){
+      // console.log(this.props.entities.questions);
+      // Object.values(this.props.entities.questions).map((question) => (
+      //   questionItems.push(<li>{question.body}</li>)
+      // ));
+      Object.values(this.props.entities.questions).map((question) => (
+          questionIndexItems.push(<QuestionIndexItem key={question.id} question={question}/>)
+      ));
+    }
+
     return (
       <section className="content-main">
         <h1>Question Feed</h1>
+        <ul>
+          {questionItems}
+        </ul>
+        <ul>
+          {questionIndexItems}
+        </ul>
       </section>
     );
   }
--- frontend/components/content/question_feed_container.jsx
+++ frontend/components/content/question_feed_container.jsx
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import QuestionFeed from './question_feed';
+import { requestQuestions } from '../../actions/session_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return{
+    entities: state.entities
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
+    requestQuestions: () => dispatch(requestQuestions())
   };
 };
 
