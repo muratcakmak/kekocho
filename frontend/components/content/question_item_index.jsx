f962345aa7ded7f5fa687996ca4dbb8594aa4462
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 10:18:39 -0400

    Apply styling

--- frontend/components/content/question_item_index.jsx
+++ frontend/components/content/question_item_index.jsx
@@ -5,13 +5,10 @@ class QuestionIndexItem extends React.Component {
     super(props);
   }
     render(){
-      debugger
     return (<div className="posts">
-
       <article className="post">
           <div className="post-body">
             <h2><a href="#">{this.props.question.body}</a></h2>
-
             <p>
               Ambo, quia consociatae gaudent Parlamenti maioritate, possint gubernium constituere. Hae duae, ut mihi videtur, differunt a nationali socialismo sive Nazismo qui olim in Germania viguit; hoc erat regimen totalitarium ubi civium libertas conculcabatur et etniae vel Judaei odio philetico vexabantur.
             </p>
