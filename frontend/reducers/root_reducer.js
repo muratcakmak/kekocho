d0e0f2c84dffc00e76eb3f7e68bf6060898785da
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-26 21:30:16 -0400

    Add entity reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import ErrorsReducer from './errors_reducer';
 import SessionReducer from './session_reducer';
-
+import EntityReducer from './entity_reducer';
 const RootReducer = combineReducers({
   session: SessionReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  entities: EntityReducer
 });
 
 
