993e4fdd46a1b4ef6a0d1f7b1a2718cf2efbea61
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 11:37:03 -0400

    Re-configure routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import NavBarContainer from './navbar/nav_bar_container';
 import Content from './content/content';
 
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 // className="session-form-container"
 const App = () => {
+  debugger
   return(
     <div>
+      <Switch>
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/signup" component={SignupFormContainer} />
-        <Route exact path="/" component={Content} />
+        <ProtectedRoute exact path="/" component={Content} />
+      </Switch>
     </div>
-  );
-};
+      );
+    };
 
-export default App;
+    export default App;
