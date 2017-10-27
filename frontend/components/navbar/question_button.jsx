7a3a9a606da8327f6201140db90d23eb7aaaf822
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 17:32:49 -0400

    frontend/components/navbar/question_button.jsx

--- frontend/components/navbar/question_button.jsx
+++ frontend/components/navbar/question_button.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 class QuestionButton extends React.Component{
+  constructor(props){
+    super(props);
+    this.showModal = this.showModal.bind(this);
+  }
+
+  showModal(){
+    this.props.addAskQuestionModal();
+  }
+
   render(){
     return (
         <div className="header-question-button-container">
-            <button className="header-question-button">Add Question</button>
+            <button onClick={this.showModal} className="header-question-button">Add Question</button>
         </div>
     );
   }
