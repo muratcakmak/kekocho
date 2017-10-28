770197fb2a20bb2a7644563e03dc4807dde3298b
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 23:46:26 -0400

    Add receive question action

--- frontend/reducers/entity_reducer.js
+++ frontend/reducers/entity_reducer.js
@@ -1,13 +1,17 @@
 import merge from 'lodash/merge';
 import { RECEIVE_ALL_QUESTIONS } from '../actions/session_actions';
+import { RECEIVE_QUESTION } from '../actions/question_actions';
 
 const defaultState = { };
 
 const EntityReducer = (state = defaultState, action) => {
-
+  debugger
   switch (action.type) {
     case RECEIVE_ALL_QUESTIONS:
       return merge({}, state, action.entities);
+    case RECEIVE_QUESTION:
+      debugger
+      return merge({}, state, { questions: action.entities });
     default:
       return state;
   }
