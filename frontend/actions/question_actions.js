998f7ef8aa3c713f88abf67d4c653ea1a7a763a4
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 23:52:34 -0400

    Create question action and utility function

---
+++ frontend/actions/question_actions.js
@@ -0,0 +1,15 @@
+import * as QuestionApiUtil from '../util/question_api_util';
+
+export const RECEIVE_QUESTION = 'RECEIVE_QUESTION';
+
+export const receiveQuestion = question => {
+  return ({
+    type: RECEIVE_QUESTION,
+    entities: { questions: question }
+  });
+};
+
+export const createQuestion = question => dispatch => {
+
+  return QuestionApiUtil.createQuestion(question).then(question => dispatch(receiveQuestion(question)));
+};
---
+++ frontend/util/question_api_util.js
@@ -0,0 +1,15 @@
+export const createQuestion = (question)=> {
+  return $.ajax({
+  	method:"post",
+  	url:"api/questions",
+  	data: { question }
+  });
+};
+
+// { body: "What is the best question?", user_id: 6}
+
+// $.ajax({
+// 	method:"post",
+// 	url:"api/questions",
+// 	data: { question: { body: "What is the best question?", user_id: 6} }
+// });
