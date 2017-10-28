15c7fdcc454de3a58b02fc2d7850ceaba78513cd
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-27 23:51:57 -0400

    Set question modal data and functions

--- frontend/components/modal/ask_question_modal.jsx
+++ frontend/components/modal/ask_question_modal.jsx
@@ -2,24 +2,51 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 class AskQuestionModal extends React.Component{
+  // question_author_id: props.userId
 
   constructor(props){
     super(props);
     this.close = this.close.bind(this);
+    this.handleQuestion = this.handleQuestion.bind(this);
+    this.state = {
+      body: "",
+      question_author_id: this.props.user.id
+    };
   }
 
   close(){
     this.props.removeAskQuestionModal();
   }
 
+  handleQuestion(e){
+    e.preventDefault();
+    this.props.createQuestion(this.state);
+    this.close();
+  }
+
+  update(field){
+    return (e)=> this.setState( { [field]: e.target.value } );
+  }
 
   render(){
     if(this.props.show){
       return (
-        <div className={"modal"}>
+        <div className={"modal-wrapper"}>
           <div className={"modal-content"}>
-            <span onClick={this.close} className="model-close">&times;</span>
-            Question Modal
+            <div>
+              <span onClick={this.close} className="model-close">&times;</span>
+            </div>
+            <div>
+              {this.props.user.email} asks
+            </div>
+            <form className="modal-form" onSubmit={this.handleQuestion}>
+              <div className="modal-header">
+                <textarea placeholder="Ask a question?" onChange={this.update("body")} className="modal-textarea"></textarea>
+              </div>
+              <div className="modal-footer">
+                <button className="session-submit-button modal-button">Post</button>
+              </div>
+            </form>
           </div>
         </div>
       );
--- frontend/components/modal/ask_question_modal_container.jsx
+++ frontend/components/modal/ask_question_modal_container.jsx
@@ -2,17 +2,23 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import AskQuestionModal from './ask_question_modal';
 import { removeAskQuestionModal } from '../../actions/ui_actions';
+import { createQuestion } from '../../actions/question_actions';
 
 const mapStateToProps = (state, ownProps) => {
-
+  if(state.session.currentUser){
+    return {
+      show: state.ui.modal.show,
+    user: state.session.currentUser};
+  }
   return{
-    show: state.ui.modal.show
+    show: state.ui.modal.show,
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    removeAskQuestionModal: () => dispatch(removeAskQuestionModal())
+    removeAskQuestionModal: () => dispatch(removeAskQuestionModal()),
+    createQuestion: (question) => dispatch(createQuestion(question))
   };
 };
 
