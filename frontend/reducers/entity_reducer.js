024d8622c9966428f798fcde8568322ebd9db023
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-28 22:41:20 -0400

    Add recieve answer

--- frontend/reducers/entity_reducer.js
+++ frontend/reducers/entity_reducer.js
@@ -1,17 +1,19 @@
 import merge from 'lodash/merge';
 import { RECEIVE_ALL_QUESTIONS } from '../actions/session_actions';
 import { RECEIVE_QUESTION } from '../actions/question_actions';
+import { RECEIVE_ANSWER } from '../actions/answer_actions';
 
 const defaultState = { };
 
 const EntityReducer = (state = defaultState, action) => {
-  
+
   switch (action.type) {
     case RECEIVE_ALL_QUESTIONS:
       return merge({}, state, action.entities);
     case RECEIVE_QUESTION:
-      
       return merge({}, state, { questions: action.entities });
+    case RECEIVE_ANSWER:
+      return merge({}, state, { answers: action.entities });
     default:
       return state;
   }
