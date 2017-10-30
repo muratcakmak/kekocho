2832051cc57e4a7e8fd7e7a588117312d6caf5e9
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-29 23:37:44 -0400

    Implement search bar

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -5,7 +5,7 @@ import GreetingContainer from '../greeting/greeting_container';
 class SearchBar extends React.Component{
   render(){
     return (
-      <input placeholder={"Search Kekocho"}></input>
+      <textarea class="selector-input text" type="text" rows="1" autofocus="True" data-group="js-editable" placeholder="Search Quora" w2cid="ks8utXH" id="__w2_ks8utXH_input" style={{ overflowX: "hidden", wordWrap: "break-word", height: "29px" }}></textarea>
     );
   }
 }
