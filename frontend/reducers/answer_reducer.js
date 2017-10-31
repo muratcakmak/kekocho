2c804a1d704509a8f63bafcb5d4f2569bdf84e9d
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-10-31 15:29:39 -0400

    Implements reducers

--- frontend/reducers/answer_reducer.js
+++ frontend/reducers/answer_reducer.js
@@ -1,5 +1,7 @@
 import merge from 'lodash/merge';
-import { RECEIVE_ANSWER } from '../actions/answer_actions';
+import difference from 'lodash/difference';
+
+import { RECEIVE_ANSWER, REMOVE_ANSWER } from '../actions/answer_actions';
 import { RECEIVE_FEED_DATA } from '../actions/feed_actions';
 
 const defaultState = { };
@@ -9,8 +11,12 @@ const AnswerReducer = (state = defaultState, action) => {
     case RECEIVE_FEED_DATA:
       return merge({}, state, action.answers);
     case RECEIVE_ANSWER:
-      
+    debugger
       return merge({}, state, action.newAnswer);
+    case REMOVE_ANSWER:
+      debugger
+      let newState = merge({}, state);
+      return newState;
     default:
       return state;
   }
--- frontend/reducers/question_reducer.js
+++ frontend/reducers/question_reducer.js
@@ -1,21 +1,32 @@
 import merge from 'lodash/merge';
-import { RECEIVE_QUESTION } from '../actions/question_actions';
+import difference from 'lodash/difference';
+
+import { RECEIVE_QUESTION, REMOVE_QUESTION } from '../actions/question_actions';
 import { RECEIVE_FEED_DATA } from '../actions/feed_actions';
-import { RECEIVE_ANSWER } from '../actions/answer_actions';
+import { RECEIVE_ANSWER, REMOVE_ANSWER } from '../actions/answer_actions';
 
 const defaultState = { };
 
 const QuestionReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+  let newState = {};
   switch (action.type) {
     case RECEIVE_FEED_DATA:
-      //This is ugly, I need to be more considerate
-      
       return merge({}, state, action.questions);
+    case REMOVE_QUESTION:
+      debugger
+      newState = merge({}, state);
+      delete newState[action.question.id];
+      return newState;
     case RECEIVE_QUESTION:
       return merge({}, state, {[action.question.id]: action.question});
     case RECEIVE_ANSWER:
-      
       return merge({}, state, action.newAnswer.questions );
+    case REMOVE_ANSWER:
+      debugger
+      newState = Object.values(merge({}, state))[0];
+      delete newState.answers[action.newAnswer.answers.id];
+      return { [Object.keys(state)]: newState };
     default:
       return state;
   }
