dc1c4765ad11f517c5ed22b33ef2d43525b7db14
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-11-05 20:23:50 -0500

    Add readmore feature

---
+++ frontend/components/question_show/read_more.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Truncate from 'react-truncate';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+
+class ReadMore extends Component {
+    constructor(...args) {
+        super(...args);
+
+        this.state = {
+            expanded: false,
+            truncated: false
+        };
+
+        this.handleTruncate = this.handleTruncate.bind(this);
+        this.toggleLines = this.toggleLines.bind(this);
+    }
+
+    handleTruncate(truncated) {
+        if (this.state.truncated !== truncated) {
+            this.setState({
+                truncated
+            });
+        }
+    }
+
+    toggleLines(event) {
+        event.preventDefault();
+
+        this.setState({
+            expanded: !this.state.expanded
+        });
+    }
+
+    render() {
+        const {
+            children,
+            more,
+            less,
+            lines
+        } = this.props;
+
+        const {
+            expanded,
+            truncated
+        } = this.state;
+        debugger
+        return (
+            <div>
+                <Truncate
+                    lines={!expanded && lines}
+                    ellipsis={(
+                        <span>... <a href='#' onClick={this.toggleLines}>{more}</a></span>
+                    )} onTruncate={this.handleTruncate}>
+                    {ReactHtmlParser(children)}
+                </Truncate>
+                {!truncated && expanded && (
+                    <span> <a href='#' onClick={this.toggleLines}>{less}</a></span>
+                )}
+            </div>);
+    }
+}
+
+ReadMore.defaultProps = {
+    lines: 3,
+    more: 'Read more',
+    less: 'Show less'
+};
+
+ReadMore.propTypes = {
+    children: PropTypes.node.isRequired,
+    text: PropTypes.node,
+    lines: PropTypes.number
+};
+
+export default ReadMore;
