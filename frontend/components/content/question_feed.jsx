99366f7681432b9c2a9a4cf1e5766164de030bcf
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-11-27 11:55:05 -0500

    Show topics questions

--- frontend/components/content/question_feed.jsx
+++ frontend/components/content/question_feed.jsx
@@ -2,30 +2,50 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import QuestionIndexItem from './question_item_index';
 import FeedAddQuestionPromptContainer from './feed_add_question_prompt_container';
-import { compare } from '../../util/util'
+import { compare } from '../../util/util';
+
 class QuestionFeed extends React.Component{
 
-  componentDidMount(){
-    this.props.requestFeedData();
+  constructor(props){
+    super(props);
+    this.state = {
+      topicId: this.props.topicId,
+    };
   }
+
+  componentDidMount(){
+    if(this.props.path.slice(0, 7) === "/topics"){
+      this.props.requestTopicQuestions(this.props.topicId);
+      this.setState({ topicId: this.props.topicId });
+    }else{
+      this.props.requestFeedData();
+    }
+  }
+
+  componentWillReceiveProps(newProps){
+    if(newProps.match.params.topicId !== this.state.topicId){
+      this.props.requestTopicQuestions(this.props.topicId);
+      this.setState({ topicId: this.props.topicId });
+    }
+  }
+
   render(){
     const questionIndexItems = [];
     const answers = this.props.answers;
     const path = this.props.path;
-    debugger
     Object.values(this.props.questions).sort(compare).map((question) => {
       const firstAnswer = answers[question.answerIds[0]];
       if(path === "/answers"){
         if(typeof firstAnswer === "undefined"){
           questionIndexItems.push(<QuestionIndexItem currentUser={this.props.currentUser} deleteQuestion={this.props.deleteQuestion} key={question.id} question={question} firstAnswer={firstAnswer}/>);
         }
+      }else if (path.slice(0,7) === "/topics") {
+        //topic
+        questionIndexItems.push(<QuestionIndexItem currentUser={this.props.currentUser} deleteQuestion={this.props.deleteQuestion} key={question.id} question={question} firstAnswer={firstAnswer}/>);
       }else{
         questionIndexItems.push(<QuestionIndexItem currentUser={this.props.currentUser} deleteQuestion={this.props.deleteQuestion} key={question.id} question={question} firstAnswer={firstAnswer}/>);
       }
     });
-
-    debugger
-
     return (
       <section className="content-main">
         <FeedAddQuestionPromptContainer />
--- frontend/components/content/question_feed_container.jsx
+++ frontend/components/content/question_feed_container.jsx
@@ -4,13 +4,15 @@ import QuestionFeed from './question_feed';
 import { requestFeedData } from '../../actions/feed_actions';
 import { deleteAnswer } from '../../actions/answer_actions';
 import { deleteQuestion } from '../../actions/question_actions';
+import { requestTopicQuestions } from '../../actions/topic_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return{
     currentUser: state.session.currentUser,
     questions: state.entities.questions,
     answers: state.entities.answers,
-    path: ownProps.history.location.pathname,
+    path: ownProps.match.path,
+    topicId: ownProps.match.params.topicId,
   };
 };
 
@@ -18,7 +20,8 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     requestFeedData: () => dispatch(requestFeedData()),
     deleteAnswer: (answerId) => dispatch(deleteAnswer(answerId)),
-    deleteQuestion: (answerId) => dispatch(deleteQuestion(answerId))
+    deleteQuestion: (answerId) => dispatch(deleteQuestion(answerId)),
+    requestTopicQuestions: (topicId) => dispatch(requestTopicQuestions(topicId)),
   };
 };
 
